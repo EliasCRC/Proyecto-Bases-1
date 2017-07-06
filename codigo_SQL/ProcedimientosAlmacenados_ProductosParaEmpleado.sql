f9dbb1f1ccb03ab81d6c9933b470d77e9ce7a5bf
JhosuaArias <jhosuarqui@hotmail.com>
2017-07-06 17:14:49 -0600

    Update ProcedimientosAlmacenados_ProductosParaEmpleado.sql

--- codigo_SQL/ProcedimientosAlmacenados_ProductosParaEmpleado.sql
+++ codigo_SQL/ProcedimientosAlmacenados_ProductosParaEmpleado.sql
@@ -3,20 +3,32 @@ use DB_GRUPO1
 Go															
 CREATE PROCEDURE modificar_Nombre_Producto
 	@nombreViejo	VARCHAR(20),
-	@nombreNuevo	VARCHAR(20)
+	@nombreNuevo	VARCHAR(20),
+	@estado			BIT	OUTPUT
 	AS
+	BEGIN TRY
 	UPDATE Producto
 	SET Nombre = @nombreNuevo
 	WHERE Nombre = @nombreViejo
+	END TRY
+	BEGIN CATCH
+		SET @estado = ERROR_MESSAGE()
+	END CATCH
 
 Go																
 CREATE PROCEDURE modificar_Precio_Producto
 	@nombre			VARCHAR(20),
-	@precio			INT
+	@precio			INT,
+	@estado			BIT	OUTPUT
 	AS
+	BEGIN TRY
 	UPDATE Producto
 	SET Precio = @precio
 	WHERE Nombre = @nombre
+	END TRY
+	BEGIN CATCH
+	SET @estado = ERROR_MESSAGE()
+	END CATCH
 
 Go
 CREATE PROCEDURE consultar_Todos_Productos
@@ -29,7 +41,6 @@ CREATE PROCEDURE consultar_Producto_Nombre_Precio
 	@nombre VARCHAR(20),
 	@precio INT
 	AS
-
 	IF @NOMBRE IS NULL BEGIN
 		SELECT *
 		FROM Producto
@@ -40,4 +51,3 @@ CREATE PROCEDURE consultar_Producto_Nombre_Precio
 		FROM Producto
 		WHERE Nombre = @nombre 
 	END
-
