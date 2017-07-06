ca7d55c35b6990fe4d575f83dee271430e45840e
Elías Calderón <eliascr98@outlook.com>
2017-07-06 17:42:19 -0600

    Pantallas seleccion de mes

---
+++ codigo_App/Seleccionar Mes Resumen Reservacion/SelecMesResumenReserv.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoBases
+{
+    public partial class SelecMesResumenReserv : Form
+    {
+        private string[] meses = new string[] {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        public SelecMesResumenReserv()
+        {
+            InitializeComponent();
+            for (int i = 0; i < meses.Length; i++)
+            {
+                comboBoxMes.Items.Add(meses[i]);
+
+            }
+            comboBoxMes.SelectedIndex = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxMes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
