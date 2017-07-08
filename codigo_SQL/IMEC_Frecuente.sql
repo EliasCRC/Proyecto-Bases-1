de0be0ef3b670d5b7fad55364f73b2da8780310f
JhosuaArias <jhosuarqui@hotmail.com>
2017-07-08 12:40:03 -0600

    Update IMEC_Frecuente.sql

--- codigo_SQL/IMEC_Frecuente.sql
+++ codigo_SQL/IMEC_Frecuente.sql
@@ -2,21 +2,48 @@ use DB_GRUPO1
 
 Go
 CREATE PROCEDURE insertar_Frecuente
-	@tel	VARCHAR(10)
+	@tel	VARCHAR(10),
+	@numR  tinyint,
+	@estado bit output
 AS
+BEGIN TRY	
 	INSERT INTO Frecuente
-	VALUES(@tel, 0);
+	VALUES(@tel, @numR);
+	END TRY
+BEGIN CATCH
+	SET @estado = ERROR_MESSAGE()
+END CATCH
 
 
 /* No hace falta un Modificar para Frecuente */
-
+GO
+CREATE PROCEDURE modificar_Frecuente
+	@telViejo 	VARCHAR(10),
+	@telNuevo	VARCHAR(10),
+	@numR  tinyint,
+	@estado bit output
+AS
+BEGIN TRY	
+	UPDATE Frecuente
+	SET Telefono = @telNuevo , NumReservAutomaticasCanceladas = @numR
+	WHERE Telefono = @telViejo
+	END TRY
+BEGIN CATCH
+	SET @estado = ERROR_MESSAGE()
+END CATCH
 
 Go
 CREATE PROCEDURE eliminar_Frecuente
-	@tel	VARCHAR(10)
+	@tel	VARCHAR(10),
+	@estado bit output
 AS
+BEGIN TRY	
 	DELETE FROM Frecuente
 	WHERE Telefono = @tel;
+	END TRY
+BEGIN CATCH
+	SET @estado = ERROR_MESSAGE()
+END CATCH
 
 
 /* El consultar del frecuente ya está abarcado en el consultar de cliente */
