1c6fd22e048d234ab1f377dacb5f6f9aafe53617
danielmdob <danielmdob@users.noreply.github.com>
2017-07-10 11:01:36 -0600

    Corrección del trigger

--- codigo_SQL/TriggerMontoTotal.sql
+++ codigo_SQL/TriggerMontoTotal.sql
@@ -1,27 +1,26 @@
-use DB_GRUPO1
-
 GO
-CREATE TRIGGER ActualizaMontoTotal
-ON Contiene AFTER INSERT
+CREATE TRIGGER Actualizar_MontoTotal
+ON Contiene AFTER INSERT, DELETE, UPDATE
 AS
-	DECLARE @nombProducto varchar(20), @momento datetime, @cantidad tinyint, @precioUnidad int
-	DECLARE cursorContiene CURSOR FOR
-		SELECT NombreProducto, MomentoVenta, Cantidad
-		FROM Contiene
-	OPEN cursorContiene
-	FETCH NEXT FROM cursorContiene INTO @nombProducto, @momento, @cantidad
-	WHILE @@FETCH_STATUS = 0 BEGIN
-		SELECT @precioUnidad = Precio
-		FROM Producto P
-		WHERE P.Nombre = @nombProducto
-		IF @precioUnidad IS NULL BEGIN
-			SET @precioUnidad = 0
-		END
-		UPDATE Venta
-		SET MontoTotal += @cantidad * @precioUnidad
-		WHERE MomentoVenta = @momento
-		FETCH NEXT FROM cursorContiene INTO @nombProducto, @momento, @cantidad
+	DECLARE  @momento datetime, @producto varchar(20), @cantidad tinyint , @MontoTotal  int = 0
+	IF EXISTS ( SELECT *
+				FROM inserted i
+	)
+	BEGIN 
+		SELECT @momento = i.MomentoVenta, @producto = i.NombreProducto, @cantidad = i.Cantidad
+		FROM	inserted i
+	END	
+	ELSE BEGIN
+		SELECT @momento = d.MomentoVenta, @producto = d.NombreProducto, @cantidad = d.Cantidad
+		FROM	deleted d
 	END
-	CLOSE cursorContiene
-	DEALLOCATE cursorContiene
-;
+	
+	IF(@producto IS NOT NULL) BEGIN
+		SELECT @MontoTotal = @MontoTotal + (Precio*Cantidad)
+		FROM Contiene C inner join Producto P on C.NombreProducto = P.Nombre 
+		WHERE @momento = C.MomentoVenta
+
+		UPDATE Venta
+		SET	 MontoTotal = @MontoTotal
+		WHERE MomentoVenta = @momento	
+	END
